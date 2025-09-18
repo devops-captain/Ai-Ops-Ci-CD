6432f6261cdbd16d49e610cd791b0b6cecf85ccd
Ashishkkasaudhan <AshishKumar.Kasaudhan@news.co.uk>
2025-09-18 13:45:44 +0530

    manual test

--- database.tf
+++ database.tf
@@ -37,14 +37,14 @@ resource "aws_security_group" "db" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["<your-vpc-cidr>/16"]
+    cidr_blocks = ["0.0.0.0/16"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "tcp"
-    cidr_blocks = ["<your-vpc-cidr>/16"]
+    cidr_blocks = ["0.0.0.0/16"]
   }
 }
 
