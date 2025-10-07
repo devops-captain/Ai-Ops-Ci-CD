45bfe5e1611edaafc3351b10318a0a3ecf4957c9
devops-captain <adevopscaptain@gmail.com>
2025-10-07 13:12:35 +0530

    Update test_terraform.tf

--- test_terraform.tf
+++ test_terraform.tf
@@ -4,18 +4,6 @@
 resource "aws_s3_bucket" "secure" {
   bucket = "secure-bucket-example"
   acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
 ################
