c445e0f597f04b80ff38d1cf2d4eb6e4da066afa
GitHub Action <action@github.com>
2025-10-11 14:47:19 +0000

    🔧 Auto-fix security issues [skip ci]

--- test_insecure.tf
+++ test_insecure.tf
@@ -1,6 +1,22 @@
 # S3 Bucket Security
 resource "aws_s3_bucket" "secure" {
   bucket = "my-secure-bucket"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  versioning {
+    enabled = true
+  }
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
 }
 
 # Security Group Rules
@@ -11,14 +27,14 @@ resource "aws_security_group" "secure" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0"] # Restrict to VPC
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # HTTPS only
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -34,7 +50,6 @@ resource "aws_db_instance" "secure" {
   manage_master_user_password = true
   vpc_security_group_ids = [aws_security_group.db.id]
 
-  # Use AWS Secrets Manager for database password
   password = data.aws_secretsmanager_secret_version.db_password.secret_string
 }
 
